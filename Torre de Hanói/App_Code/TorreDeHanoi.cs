2c6cf0c5996a2c6a50ca81da30f8e2156f0a3c76
Rafael Alcântara <ra.fa.alcantara@hotmail.com>
2018-09-25 21:57:25 -0300

    Começo do Projeto
    Código base adicionado.

---
+++ Torre de Hanói/App_Code/TorreDeHanoi.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descrição resumida de TorreDeHanoi
+/// </summary>
+public class TorreDeHanoi {
+
+    private string saida;
+
+    public TorreDeHanoi() {
+        //
+        // TODO: Adicionar lógica do construtor aqui
+        //
+    }
+
+    private void movetorre(int n, char orig, char dest, char aux) {
+        if (n == 1) {
+            saida += "<br />Mover disco 1 da torre "+orig+" para a torre "+ dest;
+            return;
+        }
+        movetorre(n - 1, orig, aux, dest);
+        saida += "<br />Mover disco "+n+" da torre " + orig + " para a torre " + dest;
+        movetorre(n - 1, aux, dest, orig);
+    }
+
+    public string main() {
+        int discos=3;
+        movetorre(discos, 'A', 'C', 'B');
+        return saida;
+    }
+}
---
+++ Torre de Hanói/TorreDeHanoi.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="TorreDeHanoi.aspx.cs" Inherits="Paginas_TorreDeHanoi" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>A Caralha da Torre de Hanói</h1>
+            <asp:Literal ID="ltr" runat="server"></asp:Literal>
+        </div>
+    </form>
+</body>
+</html>
